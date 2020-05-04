19cd9a5b71a2ccf965a6c02b514cdf4b2cc89cb4
diego-florez <diegoflorezestrada@gmail.com>
2020-05-04 18:23:49 +0200

    app deployed

--- src/app.py
+++ src/app.py
@@ -18,14 +18,14 @@ def baseResponse():
 """def userAdd():
     new_chat = {"chatId":0, "chat":request.form.get("chat_name"), "userId":0, "name":request.form.get("name"), "textId":0, "text":request.form.get("text")}
     mongo_chat = m_UserAdd(new_chat)
-    return {"Congrats! You have been added to the cartoon App": "However your chat is empty, so you wanna know new friends? Please use the following command: user/<name>/<chat_name>/recommend"}
+    return {"Congrats! You have been added to the cartoon App": "However your chat is empty, so you wanna know new friends? Please use the following command: /user/<name>/<chat_name>/recommend"}
 """
 
 @app.route("/user/<name>/<chat_name>/<text>/create", methods=["GET"])
 def userAdd(name, chat_name, text):
     new_chat = {"chatId":0, "chat":chat_name, "userId":0, "name":name, "textId":0, "text":text}
     mongo_chat = m_UserAdd(new_chat)
-    return {"Congrats! You have been added to the cartoon App": "However your chat is empty, so you wanna know new friends? Please use the following command: user/<name>/<chat_name>/recommend"}
+    return {"Congrats! You have been added to the cartoon App": "However your chat is empty, so you wanna know new friends? Please use the following command: /user/<name>/<chat_name>/recommend"}
 
 
 @app.route("/user/<name>/<chat_name>/recommend", methods=["GET"])
@@ -34,8 +34,8 @@ def recommend(name, chat_name):
     df = findUser(user, coll)
     result = getRecommend(extractColls,df)
     return {"Here you have your most similar users and their similarity with you!": result.to_dict("index")
-    , "They are cool right?": "Now you can use again the command user/<name>/<chat_name>/<text>/create again to create a chat with your friends or add them to your current chat",
-    "By the way":"If you want to analyze the sentiment of your new friends use the command /user/<phrase>/get_sentiment"}
+    , "They are cool right?": "Now you can use again the command /user/<name>/<chat_name>/<text>/create to create a chat with your friends or add them to your current chat"
+    ,"Below you see will your new friends!":"After checking them and their comments, if you want to analyze the sentiment of your new friends use the command /user/<phrase>/get_sentiment"}
     
 
 @app.route("/user/<phrase>/get_sentiment", methods=["GET"])
