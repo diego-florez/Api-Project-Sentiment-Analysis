8701d19d0a62921114c6be6054ad9bafa8f77aba
diego-florez <diegoflorezestrada@gmail.com>
2020-05-04 09:37:33 +0200

    modified

--- syncDb.sh
+++ syncDb.sh
@@ -4,7 +4,7 @@ source src/.env
 LOCALDBURI=$DBURL
 echo "WARNING!!! REMOTE DATA WILL BE DESTROYED"
 echo "Copy from $LOCALDBURI"
-echo "mongodb+srv://diego:D1eg0fl0@cartoon-api-jphao.mongodb.net/test?retryWrites=true&w=majority".freeze
+echo "mongodb+srv://diego:<ps>@cartoon-api-jphao.mongodb.net/test?retryWrites=true&w=majority"
 read REMOTEDBURI
 echo "Sync data from $LOCALDBURI to $REMOTEDBURI"
 
